cc74f7f62f9d8a0fc733283b549d211c2ab73a0f
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-21 12:50:25 +0200

    adding more in models py

---
+++ ML_CS_Pro_Backend/backend/models.py
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+# backend/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    UserID = models.AutoField(primary_key=True)
+    UserType = models.CharField(max_length=20)
+    Username = models.CharField(max_length=50, unique=True)
+    Email = models.EmailField(unique=True)
+    FullName = models.CharField(max_length=255)
+    NationalID = models.CharField(max_length=25)
+    Location = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
