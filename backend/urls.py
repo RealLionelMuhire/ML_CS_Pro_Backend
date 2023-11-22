a992d8726858353d5a458170da76b736abd152cd
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-23 00:58:29 +0200

    api updating

--- backend/urls.py
+++ backend/urls.py
@@ -1,19 +1,14 @@
 # backend/urls.py
 from django.urls import path
-from .views import home, register_admin, register_client, AdminLoginView, ClientLoginView
+from .views import home, register_admin, register_client, AdminLoginView, ClientLoginView, HelloWorldView, RegistrationView, LoginView
 
 urlpatterns = [
-    # Add a home page URL if needed
     path('', home, name='home'),
-
-    # Registration URLs
     path('register/admin/', register_admin, name='register_admin'),
     path('register/client/', register_client, name='register_client'),
-    
-
-    # Login URLs
     path('login/admin/', AdminLoginView.as_view(), name='login_admin'),
     path('login/client/', ClientLoginView.as_view(), name='login_client'),
-
-    # Add other URLs as needed
+    path('api/hello/', HelloWorldView.as_view(), name='hello_world'),
+    path('api/register/', RegistrationView.as_view(), name='registration'),
+    path('api/login/', LoginView.as_view(), name='login'),
 ]
