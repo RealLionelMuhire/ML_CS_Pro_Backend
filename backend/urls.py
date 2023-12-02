92402991d92297c443b27997cf3254b815857725
RealLionelMuhire <muhirelionel@gmail.com>
2023-12-02 21:36:28 +0200

    fixing some bug in urls to api

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import HelloWorldView, RegistrationView, login_view, logout_view,  ClientRegistrationView, ClientDeleteView, search_clients
-from .views import ListClientsView, AddFieldToClientView
+from .views import ListClientsView, AddFieldToClientView, InitiateActionView, CloseActionView
 
 urlpatterns = [
     path('api/hello/', HelloWorldView.as_view(), name='hello_world'),
@@ -12,4 +12,6 @@ urlpatterns = [
     path('api/search-clients/', search_clients, name='search-clients'),
     path('api/list-clients/', ListClientsView.as_view(), name='list-clients'),
     path('api/add-field-to-client/<int:client_id>/', AddFieldToClientView.as_view(), name='add-field-to-client'),
+    path('api/initiate-action/<int:client_id>/', InitiateActionView.as_view(), name='initiate-action'),
+    path('api/close-action/<int:action_id>/', CloseActionView.as_view(), name='close-action'),
 ]
