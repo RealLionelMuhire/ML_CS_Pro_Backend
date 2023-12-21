c61591f04d255f920acf5022c5bad3702f47844a
RealLionelMuhire <muhirelionel@gmail.com>
2023-12-21 09:53:40 +0200

    updating and username in FirstNAME in models

--- backend/models.py
+++ backend/models.py
@@ -11,34 +11,36 @@ from django.dispatch import receiver
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
+    def create_user(self, email, FirstName, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
+        user = self.model(email=email, FirstName=FirstName, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password=None, **extra_fields):
+    def create_superuser(self, email, FirstName, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, username, password, **extra_fields)
+        return self.create_user(email, FirstName, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     UserID = models.AutoField(primary_key=True)
     UserRoles = models.CharField(max_length=20)
-    username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
+    contact = models.CharField(max_length=255, blank=True, null=True)
     FirstName = models.CharField(max_length=255)
-    LastName = models.CharField(max_length=255)
+    LastName = models.CharField(max_length=255, blank=True, null=True)
     NationalID = models.CharField(max_length=25, unique=True)
-    Address = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
+    Address = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=False, null=True, blank=True)
+    is_staff = models.BooleanField(default=False, null=True, blank=True)
     registered_by_id = models.IntegerField(null=True, blank=True)
     registered_by_fullname = models.CharField(max_length=255, null=True, blank=True)
+    accessLevel = models.CharField(max_length=20, null=True, blank=True)
+    BirthDate = models.DateField(null=True, blank=True)
 
     objects = CustomUserManager()
 
