75f603d0d47b9d0ee47a2a89074197273feb2e21
RealLionelMuhire <muhirelionel@gmail.com>
2024-01-03 07:37:25 +0200

    adding alerts model

--- backend/models.py
+++ backend/models.py
@@ -181,3 +181,37 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+class Alert(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    action_taken = models.BooleanField(default=False)
+
+    # Dates
+    schedule_date = models.DateTimeField()
+    set_date = models.DateTimeField(auto_now_add=True)
+    action_taken_date = models.DateTimeField(null=True, blank=True)
+    expiration_date = models.DateTimeField()
+
+    # Empty fields
+    scheduler_name = models.CharField(max_length=255, blank=True)
+    scheduler_email = models.EmailField(blank=True)
+
+    client_name = models.CharField(max_length=255, blank=True)
+    client_email = models.EmailField(blank=True)
+    client_id = models.IntegerField(blank=True, null=True)
+
+    setter_name = models.CharField(max_length=255, blank=True)
+    setter_email = models.EmailField(blank=True)
+
+    action_taker_name = models.CharField(max_length=255, blank=True)
+    action_taker_email = models.EmailField(blank=True)
+
+    # Relationships
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='created_alerts', blank=True, null=True)
+
+    # Other related info that may be necessary
+    # ...
+
+    def __str__(self):
+        return self.title
