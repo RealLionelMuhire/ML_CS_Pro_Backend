6ae470bb049202884eaa21aadf78a846d439d487
RealLionelMuhire <RealLionel>
2024-07-24 00:43:41 +0200

    adjajusting time for token refresh

--- ML_CS_Pro_Backend/settings.py
+++ ML_CS_Pro_Backend/settings.py
@@ -186,8 +186,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=240),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': True,
     'UPDATE_LAST_LOGIN': False,
     'ALGORITHM': 'HS256',
